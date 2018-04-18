b0f8cebc994a9f795659eef7957ea62a04dbc517
Nikola Nesic <nnesic@uncc.edu>
2018-04-18 20:56:56 +0000

    Added first test scenario to test that user can see the homepage

--- spec/integration/user_accesses_the_page_spec.rb
+++ spec/integration/user_accesses_the_page_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper.rb'
 
 feature "User accesses the homepage" do
     scenario "User successfully accesses the homepage" do
-        visit generators_path
+        visit ('/')
         expect(page).to have_content("RPG Character Generator")
     end
 end
